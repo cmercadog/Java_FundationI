af408a9defd50939dd3aaa514263578739b00dc4
cmercadog <claudia_mercadog@yahoo.com>
2019-10-25 15:26:06 -0400

    Update Exercise2.java
    Fixed observation made by Juan

--- Assignment 1/Exercise2.java
+++ Assignment 1/Exercise2.java
@@ -1,3 +1,4 @@
+
 /**
  * Exercise2
  */
@@ -5,13 +6,13 @@ import java.io.Console;
 
 public class Exercise2 {
 
-    public static void main(String[] args) {
-        Console console = System.console();
-        int year = Integer.parseInt(console.readLine("Type a year: "));
-        int century = year / 100;
-        if (year % 100 != 0) 
-          century = century +1;
-        System.out.println("The Century for year " + year + " is " + century);
-          
-    }
+  public static void main(String[] args) {
+    Console console = System.console();
+    int year = Integer.parseInt(console.readLine("Type a year: "));
+    int century = year / 100;
+    if (year % 100 != 0)
+      century = century + 1;
+    System.out.println("The Century for year " + year + " is " + century);
+
+  }
 }
