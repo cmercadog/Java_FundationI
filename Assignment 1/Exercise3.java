1836b7bfe6a741148271d30b4fe5d93769a1d837
cmercadog <claudia_mercadog@yahoo.com>
2019-10-25 15:26:22 -0400

    Update Exercise3.java
    Fixed observations made by Juan

--- Assignment 1/Exercise3.java
+++ Assignment 1/Exercise3.java
@@ -1,3 +1,4 @@
+
 /**
  * Exercise3
  */
@@ -5,21 +6,20 @@ import java.io.Console;
 
 public class Exercise3 {
 
-    public static void main(String[] args) {
-        Console console= System.console();
-        String word = console.readLine("Enter a palindrom: ");
-        int start = 0;
-        int end = word.length()-1;
-        Boolean palin = true;
-        while (start <= end)
-          {
-              if (word.charAt(start) != word.charAt(end))
-                palin = false;
-              start++;
-              end--;
-                        
-          }
-        System.out.println("Is the word " + word + " entered a Palindrom? " + palin);
-
+  public static void main(String[] args) {
+    Console console = System.console();
+    String word = console.readLine("Enter a palindrom: ");
+    int start = 0;
+    int end = word.length() - 1;
+    Boolean palin = true;
+    while (start <= end) {
+      if (word.charAt(start) != word.charAt(end)) {
+        palin = false;
+        break;
+      }
+      start++;
+      end--;
     }
+    System.out.println("Is the word " + word + " entered a Palindrom? " + palin);
+  }
 }
